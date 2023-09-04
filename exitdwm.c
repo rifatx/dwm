5509faf8659d87b3561fdf0093dfd026fa69d39c
rifatx <rifat.aricanli@gmail.com>
2023-09-04 21:57:45 +0300

    lock & suspend

--- exitdwm.c
+++ exitdwm.c
@@ -74,7 +74,10 @@ void exitdwm ()
 	else if (strcmp (exit_action, S_RESTART_DWM) == 0) quit (& (const Arg) {1});
 	else if (strcmp (exit_action, S_OFFSCREEN) == 0) system ("sleep .5; xset dpms force off");
 	else if (strcmp (exit_action, S_EXIT) == 0) quit (& (const Arg) {0});
-	else if (strcmp (exit_action, S_SLEEP) == 0) system ("systemctl suspend");
+	else if (strcmp (exit_action, S_SLEEP) == 0){
+    lockscreen();
+    system ("systemctl suspend");
+  }
 	else if (strcmp (exit_action, S_REBOOT) == 0) system ("systemctl reboot");
 	else if (strcmp (exit_action, S_SHUTDOWN) == 0) system ("systemctl poweroff -i");
 
