168f3f0b69999bd218d488c57c2f332a0fff9c01
rifatx <rifat.aricanli@gmail.com>
2023-09-04 21:57:27 +0300

    modkey+shift to hide win

--- config.def.h
+++ config.def.h
@@ -103,7 +103,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_s,      show,           {0} },
 	{ MODKEY|ShiftMask,             XK_s,      showall,        {0} },
-	{ MODKEY,                       XK_h,      hide,           {0} },
+	{ MODKEY|ShiftMask,             XK_h,      hide,           {0} },
 	{ Mod1Mask,           		      XK_Tab,    altTabStart,	   {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
