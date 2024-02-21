7b47ce1e667091d4335834b888bffeedc206d704
rifatx <rifat.aricanli@gmail.com>
2024-02-21 18:31:31 +0300

    tabbed st

--- config.def.h
+++ config.def.h
@@ -89,7 +89,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "tabbed", "-c", "-r", "2", "st", "-w", "''" };
 
 #include "exitdwm.c"
 #include "shift-tools.c"
